f56efb840e77d5860b30f7d1c4ecfc23fce00c44
Visrut <visrut@incubyte.co>
2023-08-20 11:11:08 +0530

    update: change loading animation

--- app/create-survey/loading.tsx
+++ app/create-survey/loading.tsx
@@ -1,37 +1,22 @@
 "use client";
 
 import React from 'react';
-import { motion } from 'framer-motion';
 import KikiAndJiji from '../icons/KikiAndJiji';
 
-export default function Loading() {
-    const flyAnimation = {
-        initial: { x: 0, y: '100vh' },
-        animate: { x: '100vw', y: '-100vh' },
-    };
+import "./Spinner.css";
 
-    const flyTransition = {
-        duration: 3,
-        ease: 'linear',
-        repeat: Infinity,
-    };
-
+export default function Spinner() {
     return (
-        <motion.div
-            className="h-screen w-screen"
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            exit={{ opacity: 0 }}
-        >
-            <motion.div
-                className="flying-image h-full w-full"
-                variants={flyAnimation}
-                initial="initial"
-                animate="animate"
-                transition={flyTransition}
-            >
+        <div className='flex justify-center items-center h-screen'>
+            <div className="spinner">
+                <div className="border"></div>
+                <div className="border"></div>
+                <div className="border"></div>
+                <div className="border"></div>
+            </div>
+            <div className="image-container"> {/* Create a container for the image */}
                 <KikiAndJiji width="200" height="200" color="white" />
-            </motion.div>
-        </motion.div>
+            </div>
+        </div>
     );
-}
+};
