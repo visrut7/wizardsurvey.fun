55fff81a921c0d9c30afa7a19dd93d97ae4ac1c2
Visrut <visrut@incubyte.co>
2023-08-22 10:10:54 +0530

    chore(tests): setup jest

---
+++ __tests__/index.spec.tsx
@@ -0,0 +1,21 @@
+import { render, screen } from '@testing-library/react'
+import Home from "@/app/page"
+
+describe('Home', () => {
+    it('should renders a heading', () => {
+        render(<Home />)
+
+        const heading = screen.getByRole('heading', {
+            name: /WIZARD SURVEY/i,
+        })
+
+        expect(heading).toBeInTheDocument()
+    });
+
+    it('should renders 3 links', () => {
+        render(<Home />)
+
+        const links = screen.getAllByRole('link');
+        expect(links.length).toBe(3);
+    });
+})
