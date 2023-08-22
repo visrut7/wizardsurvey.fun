16cb29b81c4ac737039e3dc62f2d7ccd8baf2196
Visrut <visrut@incubyte.co>
2023-08-22 09:51:27 +0530

    feat: made home page responsive

--- app/page.tsx
+++ app/page.tsx
@@ -14,14 +14,14 @@ export default function Home() {
     transition={{ delay: 0.1, duration: 0.7 }}
     className={`h-screen flex flex-col items-center justify-center gap-y-6`}
   >
-    <section className="flex gap-x-3 items-center justify-center">
+    <section className="flex flex-col md:flex-row gap-x-3 items-center justify-center">
       <WizardSurvey width="200" height="200" color="#3e4782" />
-      <h1 className={`${patrickHand.className} text-9xl text-[#3e4782] main-title`}>WIZARD SURVEY</h1>
+      <h1 className={`${patrickHand.className} text-5xl md:text-7xl lg:text-8xl 2xl:text-9xl text-[#3e4782] main-title`}>WIZARD SURVEY</h1>
     </section>
-    <section className="flex justify-center gap-x-10 gap-y-4 flex-wrap w-1/4">
+    <section className="flex flex-col md:flex-row justify-center gap-x-10 gap-y-4 flex-wrap w-1/2 xl:w-1/3 2xl:w-1/5">
       <Link className="btn-primary" href="/create-survey">Create Manually</Link>
       <Link className="btn-gradient" href="/generate-with-ai">Generate with AI</Link>
-      <Link className="btn-primary flex gap-x-4" href="https://github.com/visrut-at-incubyte/thinkless.tech" target="_blank">
+      <Link className="btn-primary flex justify-center gap-x-2" href="https://github.com/visrut-at-incubyte/thinkless.tech" target="_blank">
         <img src="/github-mark.svg" width={20} height={30} alt="" />
         <span>Source Code</span>
       </Link>
