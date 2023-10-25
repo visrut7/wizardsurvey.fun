131c6ec6e179e9ec2db0079f1e276f8ae5c2bbb1
visrut <visrut@incubyte.co>
2023-10-25 09:48:53 +0530

    remove date type for now

--- app/models/types.ts
+++ app/models/types.ts
@@ -1,20 +1,19 @@
 export enum QuestionType {
-    MULTICHOICE = "multichoice",
-    SINGLECHOICE = "singlechoice",
-    TEXT = "text",
-    NUMBER = "number",
-    DATE = "date",
-    RATE_10 = "rate-10",
-    STAR_5 = "star-5",
-    EMOJIS = "emojis",
-    YES_OR_NO = "yes-or-no",
-    NOT_DECIDED = "",
+  MULTICHOICE = 'multichoice',
+  SINGLECHOICE = 'singlechoice',
+  TEXT = 'text',
+  NUMBER = 'number',
+  RATE_10 = 'rate-10',
+  STAR_5 = 'star-5',
+  EMOJIS = 'emojis',
+  YES_OR_NO = 'yes-or-no',
+  NOT_DECIDED = ''
 }
 
 export type Question = {
-    question: string;
-    type: QuestionType;
-    choices?: string[];
-};
+  question: string
+  type: QuestionType
+  choices?: string[]
+}
 
-export type Answer = string | number | Date | string[];
+export type Answer = string | number | string[]
