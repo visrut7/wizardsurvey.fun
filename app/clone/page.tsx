4654a11f932e48a5e7f18b9d5935f5e3959d0aeb
visrut <visrut@incubyte.co>
2023-11-04 15:40:30 +0530

    fix: loading snipper size decreased

---
+++ app/clone/page.tsx
@@ -0,0 +1,5 @@
+import LoadingSpinner from '../components/LoadingSpinner'
+
+export default function Page() {
+  return <LoadingSpinner />
+}
--- app/components/LoadingSpinner.tsx
+++ app/components/LoadingSpinner.tsx
@@ -1,22 +1,22 @@
-"use client";
+'use client'
 
-import React from 'react';
-import KikiAndJiji from '../icons/KikiAndJiji';
+import React from 'react'
+import KikiAndJiji from '../icons/KikiAndJiji'
 
-import "./Spinner.css";
+import './Spinner.css'
 
 export default function LoadingSpinner() {
-    return (
-        <div className='flex justify-center items-center h-screen'>
-            <div className="spinner">
-                <div className="border"></div>
-                <div className="border"></div>
-                <div className="border"></div>
-                <div className="border"></div>
-            </div>
-            <div className="image-container"> {/* Create a container for the image */}
-                <KikiAndJiji width="200" height="200" color="white" />
-            </div>
-        </div>
-    );
-};
+  return (
+    <div className='flex justify-center items-center h-screen'>
+      <div className='spinner'>
+        <div className='border'></div>
+        <div className='border'></div>
+        <div className='border'></div>
+        <div className='border'></div>
+      </div>
+      <div className='image-container'>
+        <KikiAndJiji color='white' />
+      </div>
+    </div>
+  )
+}
